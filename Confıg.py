58a0714418559651eebc4a22c86f3bbd96ddc901
Løse Ønline <108939077+usermusti@users.noreply.github.com>
2022-08-22 01:29:26 +0300

    Create Confıg.py

---
+++ Confıg.py
@@ -0,0 +1,21 @@
+import os #SIPAK NOOB
+class Messages():
+      HELP_MSG = [
+        ".",
+
+        "**Force Subscribe**\n__Force group members to join a specific channel before sending messages in the group.\nI will mute members if they not joined your channel and tell them to join the channel and unmute themself by pressing a button.__",
+        
+        "**Setup**\n__First of all add me in the group as admin with ban users permission and in the channel as admin.\nNote: Only creator of the group can setup me and i will leave the chat if i am not an admin in the chat.__",
+        
+        "**Commmands**\n__/ForceSubscribe - To get the current settings.\n/ForceSubscribe no/off/disable - To turn of ForceSubscribe.\n/ForceSubscribe {channel username} - To turn on and setup the channel.\n/ForceSubscribe clear - To unmute all members who muted by me.\n\nNote: /FSub is an alias of /ForceSubscribe__",
+        
+        "**Developed by @loseonline **"
+      ]
+
+      START_MSG = "**Hey [{}](tg://user?id={})**\n__I can force members to join a specific channel before writing messages in the group.\nLearn more at /forcehelp__"
+
+class Config(object):
+    APP_ID = os.environ.get('API_ID', None)
+    API_HASH = os.environ.get('API_HASH', None)
+    BOT_TOKEN = os.environ.get('TOKEN', None)
+    SUDO_USERS = 1193186807
