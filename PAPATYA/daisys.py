3f141a7359d3a02674a47bdeca4956fe9884c721
Løse Ønline <108939077+usermusti@users.noreply.github.com>
2022-08-22 02:53:15 +0300

    Create daisys.py

---
+++ PAPATYA/daisys.py
@@ -0,0 +1,13 @@
+#maked by LOSE OR DAİSY
+#Made by TEAM LOSE
+import os
+demon = os.environ.get("DEMONS")
+tigers = os.environ.get("TIGERS")
+dragon = os.environ.get("DRAGONS")
+wolves = os.environ.get("WOLVES")
+dev = os.environ.get("DEV_USERS")
+owner = os.environ.get("OWNER_USERNAME")
+
+creator = "@loseonline"
+ 
+team = "MY OFFICIAL TEAM IS TEAM LOSE 🔥"
